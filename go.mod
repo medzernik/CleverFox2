cc692fed08e9d9501be646a527ab1712856cb10b
medzernik <medzernik1@gmail.com>
2022-04-03 15:49:52 +0200

    first backend work

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module CleverFox2
 go 1.18
 
 require (
+	github.com/bwmarrin/discordgo v0.24.1-0.20220401145527-4ef1d502cd1e
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/yaml.v2 v2.4.0
 )
 
-require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	golang.org/x/sys v0.0.0-20220403020550-483a9cbc67c0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"CleverFox2/config"
+	"CleverFox2/logging"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//Initialize the config
+	config.LoadConfig()
+	logging.Log.Traceln("Loaded the config file.")
+
+	//Initialize the logging system
+	errLogging := logging.StartLogging()
+	// If the log can't be created, use stdout.
+	if errLogging != nil {
+		logrus.Errorln("Failed to log to file, using default stderr")
+	} else {
+		logging.Log.Traceln("Loaded the logging system")
+	}
+
+	logging.Log.Infoln("Starting the bot...")
+	// Create a new Discord session using the provided bot token. Panic if failed.
+	dg, err := discordgo.New("Bot " + config.Cfg.ServerInfo.ServerToken)
+	if err != nil {
+		logging.Log.Panicln("error creating Discord session: ", err)
+	}
+
+	//Get the intents that are needed
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
+	dg.Identify.Token = config.Cfg.ServerInfo.ServerToken
+	dg.Identify.LargeThreshold = 250
+
+	//responder.RegisterPlugin(dg)
+
+	// Open a websocket connection to Discord and begin listening. Panic if failed.
+	err = dg.Open()
+	if err != nil {
+		logging.Log.Panicln("Error opening the websocket!: ", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	KillSignal := make(chan os.Signal, 1)
+	signal.Notify(KillSignal, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-KillSignal
+
+	// Cleanly close down the Discord session.
+
+	err2 := dg.Close()
+	if err2 != nil {
+		logging.Log.Panicln("Error closing the session: ", err2)
+	}
+
 }
