c4fd45287c58aa6875617dab0c4a9a7f6938d44a
medzernik <medzernik1@gmail.com>
2022-04-07 00:16:26 +0200

    fix the crash for the kill command. Backend work first

--- command/command.go
+++ command/command.go
@@ -111,8 +111,16 @@ var (
 // InitializeCommands function runs to initialize commands in the given session.
 func InitializeCommands(s *discordgo.Session) {
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+				h(s, i)
+			}
+		case discordgo.InteractionMessageComponent:
+
+			if h, ok := commandHandlers[i.MessageComponentData().CustomID]; ok {
+				h(s, i)
+			}
 		}
 	})
 	Start(s)
