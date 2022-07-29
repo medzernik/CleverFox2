6109d0f27bf8534d81fd19c5bab0b97fc046c4f3
medzernik <medzernik1@gmail.com>
2022-07-29 17:42:55 +0200

    Working app quit

--- tviewsystem/tviewApp.go
+++ tviewsystem/tviewApp.go
@@ -1,8 +1,12 @@
 package tviewsystem
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"log"
 )
+
+//var modal = tview.NewModal()
 
 func StartGUI() {
 	app := tview.NewApplication()
@@ -12,8 +16,32 @@ func StartGUI() {
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("CleverFox 2 Go Edition"), 0, 1, false).
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("Status"), 5, 1, false), 0, 2, false).
 		AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (20 cols)"), 20, 1, false)
-	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
-		panic(err)
+	app.SetRoot(flex, true).EnableMouse(true)
+	modal := tview.NewModal().
+		SetText("Are you sure you want to exit?").
+		AddButtons([]string{"Cancel", "Quit"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonIndex == 1 {
+				app.Stop()
+			} else {
+				app.SetRoot(flex, true)
+			}
+		})
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		// Anything handled here will be executed on the main thread
+		switch event.Key() {
+		case tcell.KeyEsc:
+			// Exit the application
+			app.SetRoot(modal, true)
+			return nil
+		}
+
+		return event
+
+	})
+
+	if err := app.Run(); err != nil {
+		log.Fatalf("critical GUI error: %s", err)
 	}
 
 }
